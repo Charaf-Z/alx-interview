431b805c4cffbc7aa31ff699536b905aea813a8a
Charaf-Z <charafomarzitouni@gmail.com>
2024-04-24 21:46:02 +0100

    back to tow function for more clarity

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Pascal Triangle."""
+"""Pascal's Triangle."""
 
 
-# def generate_next_row(prev_row):
-#     """Create the next row of Pascal's triangle based on the previous row."""
-#     return (
-#         [1]
-#         + [prev_row[i] + prev_row[i + 1] for i in range(len(prev_row) - 1)]
-#         + [1]
-#     )
+def generate_next_row(prev_row):
+    """Generate the next row of Pascal's triangle based on the previous row."""
+    return (
+        [1]
+        + [prev_row[i] + prev_row[i + 1] for i in range(len(prev_row) - 1)]
+        + [1]
+    )
 
 
 def pascal_triangle(n):
@@ -17,12 +17,5 @@ def pascal_triangle(n):
         return []
     triangle = [[1]]
     for _ in range(1, n):
-        triangle.append(
-            [1]
-            + [
-                triangle[-1][i] + triangle[-1][i + 1]
-                for i in range(len(triangle[-1]) - 1)
-            ]
-            + [1]
-        )
+        triangle.append(generate_next_row(triangle[-1]))
     return triangle
