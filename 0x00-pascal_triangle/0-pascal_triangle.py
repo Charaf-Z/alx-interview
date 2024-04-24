3b72ed08cdcd47e843774b07e90b230c427446ee
Charaf-Z <charafomarzitouni@gmail.com>
2024-04-24 21:22:20 +0100

    remove the return types

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,16 +2,8 @@
 """Pascal Triangle."""
 
 
-def generate_next_row(prev_row: list[int]) -> list[int]:
-    """
-    Generate the next row of Pascal's triangle based on the previous row.
-
-    Args:
-        prev_row (list[int]): The previous row of Pascal's triangle.
-
-    Return:
-        list[int]: The next row of Pascal's triangle.
-    """
+def generate_next_row(prev_row):
+    """Generate the next row of Pascal's triangle based on the previous row."""
     return (
         [1]
         + [prev_row[i] + prev_row[i + 1] for i in range(len(prev_row) - 1)]
@@ -19,17 +11,8 @@ def generate_next_row(prev_row: list[int]) -> list[int]:
     )
 
 
-def pascal_triangle(n: int) -> list[list[int]]:
-    """
-    Generate Pascal's triangle up to the nth row.
-
-    Args:
-        n (int): The number of rows to generate in Pascal's triangle.
-
-    Return:
-        list[list[int]]: A list of lists representing Pascal's
-            triangle up to the nth row.
-    """
+def pascal_triangle(n):
+    """Generate Pascal's triangle up to the nth row."""
     if n <= 0:
         return []
     triangle = [[1]]
