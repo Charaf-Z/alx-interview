ac00e634fc726479f19b6d2ddd3152e3e6f1115b
Charaf-Z <charafomarzitouni@gmail.com>
2024-04-24 21:52:12 +0100

    using the recusion

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,20 +2,17 @@
 """Pascal's Triangle."""
 
 
-def generate_next_row(prev_row):
-    """Generate the next row of Pascal's triangle based on the previous row."""
-    return (
-        [1]
-        + [prev_row[i] + prev_row[i + 1] for i in range(len(prev_row) - 1)]
-        + [1]
-    )
-
-
 def pascal_triangle(n):
     """Generate Pascal's triangle up to the nth row."""
-    if n <= 0:
+    if n == 0:
         return []
-    triangle = [[1]]
-    for _ in range(1, n):
-        triangle.append(generate_next_row(triangle[-1]))
-    return triangle
+    if n == 1:
+        return [[1]]
+    new_row = [1]
+    result = pascal_triangle(n - 1)
+    last_row = result[-1]
+    for i in range(len(last_row) - 1):
+        new_row.append(last_row[i] + last_row[i + 1])
+    new_row += [1]
+    result.append(new_row)
+    return result
