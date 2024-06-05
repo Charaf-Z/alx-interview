27aa7761a35bae50c4e39bfbce1fe4c8942f5250
Charaf-Z <charafomarzitouni@gmail.com>
2024-06-05 23:00:50 +0100

    semstandard check

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 
 request(
   `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`,
   function (err, res, body) {
     if (err) throw err;
     printActor(JSON.parse(body).characters, 0);
-  },
+  }
 );
 
-function printActor(actors, idx) {
+function printActor (actors, idx) {
   if (idx === actors.length) return;
   request(actors[idx], function (err, res, body) {
     if (err) throw err;
